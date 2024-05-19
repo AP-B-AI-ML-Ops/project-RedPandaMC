16ec1e4f522e932efc50269214c0847740cc6847
RedPanda <jurreandenys@gmail.com>
2024-05-19 20:19:45 +0000

    data to database flow almost done

--- data/upload_data_to_db_flow.py
+++ data/upload_data_to_db_flow.py
@@ -1,6 +1,9 @@
 import json
 import pandas as pd
-from sqlalchemy import create_engine
+import os
+import psycopg2
+from psycopg2 import OperationalError, sql
+
 
 def convert_data_to_json(json_data_string: str):
     try:
@@ -25,9 +28,43 @@ def clean_data(df):
     df = df.drop(columns=["time"])
     df = df.rename(columns={"date": "Date", "priceUsd": "PriceUSD", 
                             "circulatingSupply": "CirculatingSupply"})
-    df["PricePerToken"] = df["PriceUSD"] / df["CirculatingSupply"]
     return df
 
-def upload_data_to_db(db_user,db_password,db_port,db_name):
-    engine = create_engine(f"postgresql://{db_user}:{db_password}@localhost:{db_port}/{db_name}")
+def prepare_database(sql_file_path, db_user, db_password, db_host, db_port, db_name):
+    try:
+        if not os.path.exists(sql_file_path):
+            raise FileNotFoundError(f"SQL file '{sql_file_path}' does not exist.")
+
+        with open(sql_file_path, encoding='utf-8', mode='r') as sql_f:
+            sql_statements = sql_f.read()
+
+        conn = psycopg2.connect(
+            dbname=db_name,
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=db_port
+        )
+        cursor = conn.cursor()
+        
+        try:
+            cursor.execute(sql_statements)
+            conn.commit()
+            print("Database preparation completed successfully.")
+        except psycopg2.Error as e:
+            conn.rollback()
+            print(f"Error executing SQL statements: {e}")
+            raise
+        finally:
+            cursor.close()
+            conn.close()
+    except FileNotFoundError as e:
+        print(f"File not found error: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+        
+
+def upload_data_to_database(db_user,db_password,db_host,db_port,db_name):
+    engine = create_engine(f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}")
     
---
+++ sql_scripts/create_schemas.sql
@@ -0,0 +1,32 @@
+DROP SCHEMA IF EXISTS training CASCADE;
+DROP SCHEMA IF EXISTS validation CASCADE;
+DROP SCHEMA IF EXISTS testing CASCADE;
+DROP SCHEMA IF EXISTS recent CASCADE;
+
+CREATE SCHEMA training;
+CREATE TABLE training.training_data (
+    "PriceUSD" FLOAT,
+    "CirculatingSupply" INTEGER,
+    "Date" TIMESTAMP,
+);
+
+CREATE SCHEMA validation;
+CREATE TABLE validation.validation_data (
+    "PriceUSD" FLOAT,
+    "CirculatingSupply" INTEGER,
+    "Date" TIMESTAMP,
+);
+
+CREATE SCHEMA testing;
+CREATE TABLE testing.testing_data (
+    "PriceUSD" FLOAT,
+    "CirculatingSupply" INTEGER,
+    "Date" TIMESTAMP,
+);
+
+CREATE SCHEMA recent;
+CREATE TABLE recent.recent_data (
+    "PriceUSD" FLOAT,
+    "CirculatingSupply" INTEGER,
+    "Date" TIMESTAMP,
+);
