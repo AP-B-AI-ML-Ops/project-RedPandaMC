fa720f3c81acbe7d49447be972ff5fa83fe69e91
RedPanda <jurreandenys@gmail.com>
2024-05-19 12:45:59 +0000

    upload data to db begin, reworked devcontainer added git graph!

--- data/download_data_from_coincap_flow.py
+++ data/download_data_from_coincap_flow.py
@@ -1,39 +1,40 @@
-"""This file contains a function which allows for the 
-gathering of historic crypto coin related data from coincap API"""
+"""This module provides a function for fetching historical data of a given cryptocurrency from the CoinCap API."""
+
 import requests
 from datetime import datetime, timedelta
 from typing import Optional, Dict, Any
 
-def get_crypto_data_flow(interval: str = "m15", crypto_coin: str = "dogecoin") -> Optional[Dict[str, Any]]:
+def get_crypto_data_flow(interval: str = "m15", crypto_coin: str = "bitcoin") -> Optional[Dict[str, Any]]:
     """
-    Fetches historical data for a given cryptocurrency.
+    Fetches historical data for a given cryptocurrency from the CoinCap API.
 
     Args:
-        interval (str): The time interval for data points, e.g., 'm1' for 1 minute, 'h1' for 1 hour, etc.
+        interval (str, optional): The time interval for data points. Defaults to 'm15' (15 minutes).
         crypto_coin (str, optional): The cryptocurrency symbol. Defaults to 'bitcoin'.
 
     Returns:
-        dict or None: A dictionary containing historical data if successful, otherwise None.
+        tuple or None: A tuple containing headers and historical data if successful, otherwise None.
     """
-    def get_data(url:str,params:dict,headers:dict,data:dict):
-        resp : requests.Response = requests.get(url, params=params, headers=headers,data=data)
+    def get_data(url:str,params:dict,headers:dict,data:dict) -> requests.Response:
+        """Send a GET request to the specified URL with provided parameters, headers, and data."""
+        resp: requests.Response = requests.get(url, params=params, headers=headers, data=data)
         return resp
 
-    url : str = "https://api.coincap.io/v2/assets/%s/history?" % crypto_coin
+    url: str = "https://api.coincap.io/v2/assets/%s/history?" % crypto_coin
     
-    end_time : int = int(datetime.now().timestamp()) * 1000
-    start_time : int = int((datetime.now() - timedelta(weeks=6)).timestamp()) * 1000
-    params : dict = {
+    end_time: int = int(datetime.now().timestamp()) * 1000
+    start_time: int = int((datetime.now() - timedelta(weeks=6)).timestamp()) * 1000
+    params: dict = {
         "interval": interval,
         "start": start_time,
         "end": end_time
     }
 
-    headers : dict = {}
+    headers: dict = {}
 
-    payload : dict = {}
+    payload: dict = {}
 
-    response : requests.Response = get_data(url,params,headers,payload)
+    response: requests.Response = get_data(url, params, headers, payload)
     
     if response.status_code == 200:
         return response.headers, response.text
---
+++ data/upload_data_to_db_flow.py
@@ -0,0 +1,13 @@
+def convert_data_to_json():
+    pass
+
+def convert_json_to_df():
+    pass
+
+def clean_data():
+    pass
+
+def upload_data_to_db():
+    pass
+
+# 🩵🩷🤍🩷🩵
