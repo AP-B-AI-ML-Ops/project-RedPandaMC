c91cae35b15130669c182f0a89447bf73b9de029
JurreanDeNys <jurreandenys@gmail.com>
2024-05-24 10:51:33 +0000

    updated main_flows.py

--- main_flows.py
+++ main_flows.py
@@ -1,13 +1,18 @@
+"""
+In this file you'll find all the entrypoints 
+for the different deployments for this project
+"""
 import yaml
 
-config_file_path = 'cryptopredictor_config.yml'
+CONFIG_FILE_PATH = 'cryptopredictor_config.yml'
 
 def read_config(file_path):
-    with open(file_path, 'r') as file:
-        config = yaml.safe_load(file)
-    return config
+    """Reads the config file"""
+    with open(file_path, 'r',encoding='utf-8') as file:
+        conf = yaml.safe_load(file)
+    return conf
 
-config = read_config(config_file_path)
+config = read_config(CONFIG_FILE_PATH)
 
 model_parameters = config.get('model_parameters', {})
 training_range = model_parameters.get('training_range', {})
@@ -17,12 +22,3 @@ interval = model_parameters.get('interval', {})
 num_values = model_parameters.get('num_values')
 threshold = model_parameters.get('threshold')
 crypto_coin = model_parameters.get('crypto_coin')
-
-print("Model Parameters:")
-print(f"Training Range: {training_range}")
-print(f"Validation Range: {validation_range}")
-print(f"Test Range: {test_range}")
-print(f"Interval: {interval}")
-print(f"Number of Values: {num_values}")
-print(f"Threshold: {threshold}")
-print(f"Crypto Coin: {crypto_coin}")
